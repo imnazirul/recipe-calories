e97b208f80506f11790c7fac0427bb398211bb1d
nazirul <nazirul.me01c@gmail.com>
2024-03-17 09:07:57 +0600

    some bug fixed

--- src/components/TableRow/TableRow.jsx
+++ src/components/TableRow/TableRow.jsx
@@ -5,7 +5,7 @@ const TableRow = ({ data, index, handlePreparing }) => {
   const { recipe_name, calories, preparing_time } = data;
 
   return (
-    <tr className="text-[#282828B3] bg-[#2828280D] flex ">
+    <tr className="text-[#282828B3] bg-[#2828280D]  ">
       <td className="flex items-center">
         <span className="text-[#282828CC] font-semibold mr-3">{index + 1}</span>
         <p className="max-w-24">{recipe_name}</p>
